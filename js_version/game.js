994cd952b67b9fba941e43114d17053bb334a1de
Br34th7aking <lightbulb.holder@gmail.com>
2019-06-04 23:28:12 +0530

    Add response on button click

--- js_version/game.js
+++ js_version/game.js
@@ -3,6 +3,12 @@ const startButton = document.querySelector('.start-button');
 const gameScreen = document.querySelector('.game-screen');
 let playerNameInput = document.querySelector('.player-name');
 
+
+
+// doors 
+const doors = document.querySelectorAll('.door');
+
+let messageScreen = document.querySelector('.message-screen');
 let playerName = 'Adventurer';
 
 startButton.addEventListener('click', () => {
@@ -12,4 +18,12 @@ startButton.addEventListener('click', () => {
 
     welcomeScreen.style.display = "none";
     gameScreen.style.display = "block"; 
+    messageScreen.innerHTML = playerName + ', you see three doors. Which one will you open?(Click your choice)'
 });
+
+for (let i = 0; i < doors.length; i++) {
+        doors[i].addEventListener('click', () => {
+            messageScreen.innerHTML = 'You chose to open ' + doors[i].id; 
+        });
+}
+
